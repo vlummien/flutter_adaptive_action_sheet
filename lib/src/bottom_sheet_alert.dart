7d8cb44d273bc4f9dc6ae9e15195b25477831ae7
Julian Macias De La Rosa <julian@appdafuer.com>
2021-09-26 10:35:31 +0200

    close bottom sheet when tapped on action

--- lib/src/bottom_sheet_alert.dart
+++ lib/src/bottom_sheet_alert.dart
@@ -86,7 +86,10 @@ Future<T?> _showCupertinoBottomSheet<T>(
           return Material(
             color: Colors.transparent,
             child: CupertinoActionSheetAction(
-              onPressed: action.onPressed,
+              onPressed: () {
+                action.onPressed();
+                Navigator.of(coxt).pop();
+                },
               child: Row(
                 children: [
                   if (action.leading != null) ...[
@@ -201,7 +204,11 @@ Future<T?> _showMaterialBottomSheet<T>(
               if (cancelAction != null)
                 InkWell(
                   onTap:
-                      cancelAction.onPressed ?? () => Navigator.of(coxt).pop(),
+                      (){
+                        cancelAction.onPressed();
+                        Navigator.of(coxt).pop();
+                       }
+                        ?? () => Navigator.of(coxt).pop(),
                   child: Center(
                     child: Padding(
                       padding: const EdgeInsets.all(16.0),
